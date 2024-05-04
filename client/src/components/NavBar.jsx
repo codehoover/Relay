966fb64e8956bd2091502a37c56d323f5454897e
Esau R <mumbonumber7@gmail.com>
2024-05-03 22:37:01 -0500

    pushing changes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Link, Navigate, json, redirect } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import Relay from '../assets/Relay.png';
 
 export default function NavBar(){
     const {setUserInfo, userInfo} = useContext(UserContext);
@@ -31,10 +32,8 @@ export default function NavBar(){
     const username = userInfo?.username;
     return(
         <header className="nav-container">
-            <Link to="/">             
-                <svg  className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 403.44 74.33">
-                    <path class="squiggle" fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="48" d="M14.2 65.54s36-36.79 56.31-35.66 38.6 27.31 58.13 26.56 26.34-5.91 37.6-13 30.53-19.52 39.48-19.14 19.48 8.23 31.12 19.87 18.91 13.2 25.25 13.16S278.24 58 297 40.76s25.86-17.86 31.49-17.86 48.11 15.42 65 13.27" />
-                </svg>     
+            <Link to="/" className="logo-header">             
+                <img src={Relay} className="logo"/>  
             </Link>
 
             <div className="nav-sec-2">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,236 @@
 import Post from "../components/Post";
 import Dune from "../assets/Dune-2-title.jpg"; 
 import { Link } from "react-router-dom";
+import Stamp from "../assets/stamp.png";
 export default function Home(){
     return(
         <div className="home-container">
             <div className="hero-banner">
-                <p className="tagline">
-                    <strong>
-                        Welcome to <span className="relay">Relay.</span>
-                    </strong> 
-                    Inspire, Learn, Grow. 
-                </p>
-                <p style={{fontSize:"20px", width:"425px"}}> Write, share and read about the ideas and topics that you care about. The only limit is your imagination.</p>
-                <Link to={"/Register"}> Get started </Link>
+
+                <div className="hero-sec-1">
+                    <p className="tagline">
+                        <strong>
+                            Welcome to <span className="relay">Relay.</span>
+                        </strong> 
+                        Inspire, Learn, Grow. 
+                    </p>
+                    <p style={{fontSize:"20px", width:"425px"}}> Write, share and read about the ideas and topics that you care about. The only limit is your imagination.</p>
+                    <Link to={"/Register"}> Get started </Link>
+                </div>
+
+                <div className="hero-sec-2">
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/> 
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                    <img id="stamp" src={Stamp}/>
+                </div>
+
             </div>
 
             <div className="home-posts-container">
